818e1e22beac227be6f10185838cfbccc47c0e44
Hosamaneprashanth <prashanth.18035@gmail.com>
2023-09-29 22:19:23 +0530

    first commit

---
+++ src/default1/Test1.java
@@ -0,0 +1,14 @@
+package default1;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("hi prashanth");
+		String hungry ="yes";
+		if (hungry=="yes") {
+System.out.println("eat biriyani");
+		}
+		else {
+System.out.println("do not eat anything");
+	}
+
+}}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author N Rajashekar
+ *
+ */
+module Gitdemopriojectversioni1 {
+}
